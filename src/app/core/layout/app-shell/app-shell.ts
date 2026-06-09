60576236435f6841e0f0ded29b13cae0ba9b1b3e
Aydin Kaya <kayaan76@googlemail.com>
2026-06-09 22:33:31 +0200

    clear app shell and create initial flight list page

--- src/app/core/layout/app-shell/app-shell.ts
+++ src/app/core/layout/app-shell/app-shell.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-app-shell',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  selector: 'app-shell',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
 })
