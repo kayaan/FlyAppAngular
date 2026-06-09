58e3b110d98ee328d41c645a67f7550a232b50f2
Aydin Kaya <kayaan76@googlemail.com>
2026-06-09 21:56:38 +0200

    no need for seperate flight import page

--- src/app/features/flights/pages/flight-import/flight-import.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-flight-import',
-  imports: [],
-  templateUrl: './flight-import.html',
-  styleUrl: './flight-import.scss',
-})
-export class FlightImport {}
